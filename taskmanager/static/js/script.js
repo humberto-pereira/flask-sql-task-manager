b1081b4b65537657d5a89649c6429527e059679d
Humberto Pereira <humberto_pereira@live.com>
2023-09-20 19:04:51 +0000

    add  task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let  datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, options);
-});
