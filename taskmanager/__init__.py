faa92c7ec920ff4632925213d9ca072d5f7bf7d5
Humberto Pereira <humberto_pereira@live.com>
2023-09-23 20:50:29 +0000

    commit

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -15,7 +15,6 @@ else:
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
-
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
